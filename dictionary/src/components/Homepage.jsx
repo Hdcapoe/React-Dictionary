739217d91df785b98936bd199832510eff7b7e5c
Hdcapoe <jamal223hd@gmail.com>
2024-01-09 05:07:21 -0500

    3

--- dictionary/src/components/Homepage.jsx
+++ dictionary/src/components/Homepage.jsx
@@ -3,6 +3,14 @@ const Homepage = () => {
 	const [definition, setDefinition] = useState([]);
 	const [searchedWord, setSearchedWord] = useState('');
 
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '393d3bf389msh22ad968edc3f86ep10bee4jsncc2d901b34cf',
+			'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com',
+		},
+	};
+
 	const apiUrl =
 		'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary';
 
