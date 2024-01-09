2456214e4b7bc716055c8b284e10bd9ec086cee3
Hdcapoe <jamal223hd@gmail.com>
2024-01-09 04:41:19 -0500

    first commit

--- dictionary/src/components/Homepage.jsx
+++ dictionary/src/components/Homepage.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react'
-import { useState, useEffect } from "react";
-
+import React from 'react'
 
 const Homepage = () => {
-
-const [word, setword] = useState([])
-
-async function getData() {
-  try {
-    let response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/<word>')
-
-  } catch (error) {
-    
-  }
-}
-
-
-
   return (
     <div>Homepage</div>
-
-    
   )
 }
 
